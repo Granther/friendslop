20c8341b7c325420af3da113c7be08fd308029d8
Asfar Jamil <asfarj@withnubo.com>
2026-03-15 12:41:59 -0400

    style: animation work, player character

---
+++ Scripts/PlayerLowerbody.gd
@@ -0,0 +1,10 @@
+extends Node3D
+
+# Called when the node enters the scene tree for the first time.
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	 
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		stonemanHeadless.rotate_y(head.rotatio)
