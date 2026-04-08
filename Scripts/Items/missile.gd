fcaa2687e46ba6a595f0d3d8b77e3c3ab9b86cd9
Grant Herrema <granther321@gmail.com>
2026-04-08 16:47:03 -0400

    feature: fine tuned missile systems, kind of goofy but it works

--- Scripts/Items/missile.gd
+++ Scripts/Items/missile.gd
@@ -23,20 +23,24 @@ func node_added(node: Node3D):
 func seek():
 	var steer_vec = Vector3.ZERO
 	if lock:
+		# Desired is the direct psitional difference between both, magnified by speed
 		var desired = (target.position - position).normalized() * speed
+		# Our steer vec is the force to move our current direction to the desired one, this grows with steer_force
 		steer_vec = (desired - velocity).normalized() * steer_force
-	return steer_vec
+		return steer_vec
+	else:
+		return Vector3(0,1,0)
 
 func _physics_process(delta: float) -> void:
-	accel += seek()
-	velocity += accel * delta
+	# When we += our accel with see(), we gat a wave because as we grow away, since we are adding,we grow further, so seek() keep growing directionally. We pass it and accel starts going in the opposite sign, so we hit a peak, then begin heading down, but since we spend more time going down, the accel grows more
+	velocity += seek() * delta
 	## velocity.clamp(1, speed)
 	##rotation = velocity.angle_to
 	## rotation = velocity.normalized()
 	position += velocity * delta
 	#ray.rotation = direction
-	if target != null:
-		look_at(velocity, Vector3(0,0,1))
+	#if target != null:
+		#look_at(velocity, Vector3(0,0,1))
 	
 func _on_get_lock_timer_timeout() -> void:
 	lock = true
--- Scripts/Items/missile_2.gd
+++ Scripts/Items/missile_2.gd
@@ -4,9 +4,11 @@ extends RigidBody3D
 # Maybe get more accurate as it gets closer??
 @export var steer_force: float = 1
 @export var target: Node3D = null
+@export var lock_time: float
+@export var launch_time: float 
 
 var velocity = Vector3.ZERO
-var accel = Vector3(0, 9.8*1.2, 0)
+var accel = Vector3(0, 0, 0)
 var direction
 var locked: bool = false
 var rot = Vector3()
@@ -14,64 +16,93 @@ var distance
 var start_dist
 
 @onready var ray = $RayCast3D
-@onready var timer = $LockTimer
+@onready var lock_timer = $LockTimer
+@onready var launch_timer = $LaunchTimer
+@onready var motor_timer = $MotorTimer
 @onready var anim_player = $AnimatedSprite3D
 @onready var coll_shape = $CollisionShape3D
 @onready var mesh_inst = $MeshInstance3D
+@onready var rng = RandomNumberGenerator.new()
+@onready var kill_area = $KillArea
 
 func _ready():
+	launch_timer.wait_time = launch_time
+	lock_timer.wait_time = lock_time
 	anim_player.hide()
-	get_tree().node_added.connect(node_added)
-	timer.start()
+	if target == null:
+		get_tree().node_added.connect(node_added)
+	set_physics_process(false)
+	launch_timer.start()
+
+func get_motor_accel():
+	#var motor_accel = pow(9.8, ((motor_timer.wait_time - motor_timer.time_left)/4))
+	#print("Notor accel: ", motor_accel, motor_timer.wait_time - motor_timer.time_left)
+	#return Vector3(0, motor_accel, 0)
+	return Vector3(0,9.8*5.5,0)
 
 func node_added(node: Node3D):
 	if node is CharacterBody3D:
 		target = node
 
 func seek():
+	# var motor_accel = Vector3(0,9.8*5.5,0)
 	var steer_vec = Vector3.ZERO
 	if locked:
 		var desired = (target.position - position).normalized() * speed
+		#if rng.randf_range(0, 100) > 60:
+			#var r = 60
+			#var rand_vec = Vector3(rng.randf_range(-r, r), rng.randf_range(-r, r), rng.randf_range(-r, r))
+			#desired += rand_vec
 		# This is our "Course correction"
+		# We grow the magnitude based off the steer force
 		steer_vec = (desired - velocity).normalized() * steer_force
-	return steer_vec
+		return steer_vec + get_motor_accel()
+	else:
+		return get_motor_accel()
 
 # We have a target and are locked, so we get the accel 
 
 func _physics_process(delta: float) -> void:
-	if target != null and locked:
+	#if target != null and locked:
 		#var dir = target.position - position
 		#
 		#
 		## If zero, we do no course correction
-		#accel += seek()
-	#velocity += accel * delta
-	#position += velocity * delta
+	velocity += seek() * delta
+	# position += velocity * delta
+	global_translate(velocity * delta)
 		# We want the vector to be in the direction of the target
 		# target, but remove the direction of our position
-		direction = target.position - position
-		direction = direction.normalized()
-		var rotAmount = direction.cross(global_transform.basis.z)
-		rot.y = rotAmount.y * steer_force * delta
-		rot.x = rotAmount.x * steer_force * delta
+	if locked:
+		var desired = (target.position - position).normalized() * speed
+		var steer_vec = (desired - velocity).normalized() * steer_force
+		var rotAmount = velocity.normalized().cross(global_transform.basis.z)
+		rot.y = rotAmount.y
+		rot.x = rotAmount.x
 		rotate(Vector3.UP, rot.y)
 		rotate(Vector3.RIGHT, rot.x)
-		
-	# -global_transform.basis.z is our forward, so we are merely setting vclocity
-	global_translate(-global_transform.basis.z * speed * delta)
+		#
+	## -global_transform.basis.z is our forward, so we are merely setting vclocity
+	#global_translate(-global_transform.basis.z * speed * delta)
+
+func _on_launch_timer_timeout() -> void:
+	lock_timer.start()
+	motor_timer.start()
+	set_physics_process(true)
 
 func _on_lock_timer_timeout() -> void:
+	kill_area.set_collision_mask_value(1, true)
 	locked = true
 	start_dist = (target.position - position).length()
 
 func _on_kill_area_body_entered(body: Node3D) -> void:
-	# mesh_inst.hide()
+	mesh_inst.hide()
 	set_physics_process(false)
-	# coll_shape.set_deferred("disabled", true)
+	coll_shape.set_deferred("disabled", true)
 	anim_player.show()
 	anim_player.play("explosion2")
 	await anim_player.animation_finished
-	# queue_free()
+	queue_free()
 
 #extends RigidBody3D
 #
