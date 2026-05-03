b06eb9b72ef7f3634fddfeabb5f486d977a3164a
Grant Herrema <granther321@gmail.com>
2026-05-03 00:23:16 -0400

    added nohope files, not impl yet

---
+++ Scripts/Player/nhope/player.gd
@@ -0,0 +1,172 @@
+extends CharacterBody3D
+signal grabbed(object)
+
+var SPEED = 0
+const SPRINT = false
+const JUMP_VELOCITY = 4.5
+const SENSITIVITY = 0.003
+var BASE_FOV = 120.0
+var FOV_CHANGE = 1
+var PUSH_FORCE = 4
+
+@onready var head = $Head
+# Yeah... These routes look ridiculous, sorry, but, it works. This is so the camera follows the neck of the ragdoll basically. I will clean this up later(tm)
+@onready var grabbed_anchor = $"Head/Stoneman/Armature/Skeleton3D/PhysicalBoneSimulator3D/Physical Bone Neck/Camera3D/SpringArm3D/GrabbedAnchor"
+@onready var object_grabber_shape_cast = $"Head/Stoneman/Armature/Skeleton3D/PhysicalBoneSimulator3D/Physical Bone Neck/Camera3D/ObjectGrabberShapeCast"
+@onready var camera = $"Head/Stoneman/Armature/Skeleton3D/PhysicalBoneSimulator3D/Physical Bone Neck/Camera3D"
+@onready var animations = $AnimationPlayer
+@onready var nametag = $"Head/Stoneman/Armature/Skeleton3D/PhysicalBoneSimulator3D/Physical Bone Upper Chest/Nametag"
+@export var grabbed_object:RigidBody3D = null
+@onready var stoneman = $Head/Stoneman
+@onready var ragdoll = $Head/Stoneman/Armature/Skeleton3D/PhysicalBoneSimulator3D
+@onready var springarm = $"Head/Stoneman/Armature/Skeleton3D/PhysicalBoneSimulator3D/Physical Bone Neck/Camera3D/SpringArm3D"
+
+# Is this ok? With godot I feel like I'm a fricken monkey electrician, just wiring up stuff as I go
+# Is there any real best practice here? Is it modular? Does it matter if it isnt?
+# @onready var ui = UIHandler.get_ui()
+# get_parent().find_child("MainUI")
+#  # "$World/MainUI"
+@onready var sync = $MultiplayerSynchronizer
+var grenade_expl: bool = false
+var val: Vector3
+
+func _enter_tree() -> void:
+	set_multiplayer_authority(str(name).to_int())
+	assert(get_collision_layer_value(4), "must be on 'Expodables' (4) collision layer")
+
+func _ready():
+	# Setting nametag of player, be sure to enable billboarding in the flags so it follows the camera
+	nametag.text = "Player: " + str(name)
+	# Ensures that the spawned characters have default animation blends when spawned in
+	stoneman.set_default_anim_blends()
+	if not is_multiplayer_authority(): return
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	camera.current = true
+
+func _input(event: InputEvent) -> void:
+	if not is_multiplayer_authority(): return
+	#if ui.is_menu_open(): return
+	if event is InputEventMouseButton:
+		if Input.is_action_just_pressed("interact"):
+			if grabbed_object:
+				throw_object()
+			elif object_grabber_shape_cast.is_colliding():
+				var collided = object_grabber_shape_cast.get_collision_result()[0]["collider"]
+				if collided is Ball:
+					if !grabbed_object:
+						try_grabbing(collided)
+
+func try_grabbing(collided:RigidBody3D):
+	grabbed_object = collided
+	grabbed_object.set_collision_layer_value(3,0)
+	grabbed_object.set_collision_mask_value(2,0)
+	emit_signal("grabbed",grabbed_object)
+	
+func throw_object():
+	# Alright, got something working for a sort of lob effect with the ball.
+	# Two factors are considered for how far and what angle the item can be thrown: 
+	# - Player Velocity
+	# - Camera Y Angle
+	var throw_angle = clamp(velocity.length() * (camera.rotation.x * 3), -50, 50)
+	grabbed_object.apply_impulse((-head.global_basis.z * velocity.length()*2) + Vector3(0,throw_angle,0))
+	grabbed_object.set_collision_layer_value(3,1)
+	grabbed_object.set_collision_mask_value(2,1)
+	grabbed_object = null
+	emit_signal("grabbed",grabbed_object)
+	pass
+
+# Why does head move on y but camera on x
+# Oh! Cause we dont want the whole player to point downward, but we want him to spin
+func _unhandled_input(event):
+	if not is_multiplayer_authority(): return
+	#if ui.is_menu_open(): return
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * SENSITIVITY)
+		camera.rotate_x(event.relative.y * SENSITIVITY)
+		camera.rotation.x = clamp(camera.rotation.x, 0.2, 1.6)
+	
+
+
+func _physics_process(delta: float) -> void:
+	springarm.set("spring_length", clamp(-camera.rotation.x,0.6,0.7))
+	
+	if not is_multiplayer_authority(): return
+	# Bug: Opening menu pauses you in game. Turns off physics lol
+	#if ui.is_menu_open(): return
+	var velocity_clamp = clamp(velocity.length(), SPEED/4.5, SPEED/4.5)
+	var target_fov = BASE_FOV * FOV_CHANGE * velocity_clamp
+	camera.fov = lerp(camera.fov, target_fov, delta * 4)
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	
+	if Input.is_action_pressed("ToggleMouseFocus"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y += JUMP_VELOCITY
+		
+	if Input.is_action_pressed("sprint"):
+		SPEED = 5
+		FOV_CHANGE = 0.7
+	else:
+		SPEED = 3
+		FOV_CHANGE = 1
+		
+	var move_direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if !ragdoll.is_simulating_physics():
+		if move_direction and is_on_floor():
+			velocity.x = lerp(velocity.x, move_direction.x * SPEED, delta * 6) 
+			velocity.z = lerp(velocity.z, move_direction.z * SPEED, delta * 6) 
+		elif is_on_floor():
+			velocity.x = velocity.x/1.2
+			velocity.z = velocity.z/1.2
+		elif not is_on_floor():
+			velocity.x = lerp(velocity.x, move_direction.x * SPEED, delta * 0.5) 
+			velocity.z = lerp(velocity.z, move_direction.z * SPEED, delta * 0.5) 
+	else:
+		velocity = $"Head/Stoneman/Armature/Skeleton3D/PhysicalBoneSimulator3D/Physical Bone Upper Chest".linear_velocity
+	
+	# Hmmm, so, we essentially have a state machine, but I'm not into that, 
+	# So, 
+	#if grenade_expl:
+		#grenade_expl = false
+		#print("Player val: ", velocity, " Val: ", val, " Total: ", (velocity + val))
+		#velocity = (velocity + val*5)
+		#
+	move_and_slide()
+	
+	for i in get_slide_collision_count():
+		var c = get_slide_collision(i)
+		if c.get_collider() is RigidBody3D:
+			c.get_collider().apply_central_impulse(-c.get_normal() * PUSH_FORCE)
+			
+	if grabbed_object:
+		var target_pos:Vector3 = grabbed_anchor.global_position
+		
+		var current_pos:Vector3 = grabbed_object.global_position
+		
+		var direction = target_pos - current_pos
+		
+		var required_velocity = direction / delta
+		
+		var velocity_correction = required_velocity - grabbed_object.linear_velocity
+		grabbed_object.linear_velocity = required_velocity
+	proc_anims(velocity)
+
+# This eventually leads to an all-peer rpc call which notifies all peers
+func proc_anims(velocity):
+	# Interesting, so we get the hypotenuse, want to know how extreme, returns scalar then
+	var animationScale = clamp(velocity.length()*2, 0, 15)
+	var animationSpeed = clamp(velocity.length()/2, 0, 1)
+	stoneman.play_walk_anims(animationScale, animationSpeed)
+
+func get_exploded(source: Vector3):
+	# Get difference between player pos and grenade pos (this is our direction relative to grenade)
+	var force = (global_transform.origin - source).normalized()*5
+	print(force.length())
+	velocity = (velocity + force)
+	
+# So, our force is the [0,1] of the difference 
+# If a = [0.5]
+# and b = [0.2]. c = [0.3], cn = 0.3/1 = 0.3 
---
+++ Scripts/Player/nhope/stoneman.gd
@@ -0,0 +1,114 @@
+extends Node3D
+
+var POLAR = false
+
+@onready var leg_animations = $"LegAnimTree"
+@onready var arm_animations = $"ArmAnimTree"
+@onready var animation_player = $LegAnimPlayer
+@onready var character = get_parent().get_parent()
+@onready var right_arm = $"Right Arm Target"
+@onready var left_arm = $"Left Arm Target"
+@onready var ragdoll = $Armature/Skeleton3D/PhysicalBoneSimulator3D
+@onready var box_collision  = $"../../CollisionShape3D"
+@onready var head_cam = $"Armature/Skeleton3D/PhysicalBoneSimulator3D/Physical Bone Neck/Camera3D"
+@onready var head = $Armature/Skeleton3D/Head
+@onready var animated_skel = $Armature/Skeleton3D
+@export var angular_spring_stiffness: float = 4000.0
+@export var angular_spring_damping: float = 80.0
+@export var max_angular_force: float = 9999.0
+var grabbed_object = null
+var physics_bones = []
+
+func _ready():
+	ragdoll.physical_bones_start_simulation()
+	ragdoll.influence = 0
+	physics_bones = ragdoll.get_children().filter(func(x): return x is PhysicalBone3D) # get all the physical bones
+
+#func _physics_process(delta):
+	#
+	#for b:PhysicalBone3D in physics_bones:
+		#var target_transform: Transform3D = animated_skel.global_transform * animated_skel.get_bone_global_pose(b.get_bone_id())
+		#var current_transform: Transform3D = physical_skel.global_transform * animated_skel.get_bone_global_pose(b.get_bone_id())
+
+func _process(delta):
+	ragdoll.influence = POLAR
+	if ragdoll.influence == 0:
+		var upper_leg = $"Armature/Skeleton3D/PhysicalBoneSimulator3D/Physical Bone Upper Leg_r"
+		var lower_leg = $"Armature/Skeleton3D/PhysicalBoneSimulator3D/Physical Bone Lower Leg_r"
+		var upper_leg_a = $"Armature/Skeleton3D/Upper Leg_r"
+		var lower_leg_a = $"Armature/Skeleton3D/Lower Leg_r"
+		upper_leg.transform = upper_leg_a.transform
+		lower_leg.transform = lower_leg_a.transform
+		ragdoll.physical_bones_stop_simulation()		
+	elif POLAR == true:
+		
+		ragdoll.influence = 1
+		ragdoll.physical_bones_start_simulation()
+
+func toggleRagdoll():
+	#head_cam.visible = !head_cam.visible
+	#head.visible = !head.visible
+	#ragdoll.active = !ragdoll.active
+	if Input.is_action_just_pressed("ragdoll"):
+		
+		POLAR = !POLAR
+		
+func hookes_law(displacement: Vector3, current_velocity: Vector3, stiffness: float, dampening: float) -> Vector3:
+	return (stiffness * displacement) - (dampening * current_velocity)
+
+func _physics_process(delta: float) -> void:
+	for b:PhysicalBone3D in physics_bones:
+		var target_transform: Transform3D = animated_skel.global_transform * animated_skel.get_bone_global_pose(b.get_bone_id())
+		var current_transform: Transform3D = ragdoll.global_transform * animated_skel.get_bone_global_pose(b.get_bone_id())
+		var rotation_difference: Basis = (target_transform.basis * current_transform.basis.inverse())
+		
+		
+	if not is_multiplayer_authority(): return
+	if grabbed_object == null:
+		set_anim_players.rpc(true)
+		set_anim.rpc("parameters/Idle/blend_amount", 0)
+	else:
+		left_arm.global_position = grabbed_object.global_position
+		right_arm.global_position = grabbed_object.global_position
+		set_anim_players.rpc(false)
+		set_anim.rpc("parameters/Idle/blend_amount", 1)
+		
+	#if ragdoll.is_simulating_physics():
+		#position = ragdoll.global_position
+	
+
+# Ok, so when I have the rpc decorator here and not on the individual funcs, it does not work
+# I think this is partially because the the character.is_on_floor() method doesn't work
+# I think this is cause the required data for that is not synced, and cannot be synced (easily)
+# so, having a set_anim method which always considers the player's animation player works well
+#@rpc("any_peer", "call_local")
+func _input(event: InputEvent) -> void:
+	if not is_multiplayer_authority(): return
+	if Input.is_action_just_pressed("ragdoll"):
+		toggleRagdoll()
+
+func set_default_anim_blends():
+	set_anim.rpc("parameters/WalkSpeed/scale", 0)
+	set_anim.rpc("parameters/Blend2/blend_amount", 0)
+
+func play_walk_anims(animationScale: float, animationSpeed: float):
+	if !character.is_on_floor() or POLAR == true:
+		set_anim.rpc("parameters/JumpBlend/blend_amount", 0.2)
+		animationSpeed = 1
+	else:
+		set_anim.rpc("parameters/JumpBlend/blend_amount", 0.7)
+	set_anim.rpc("parameters/WalkSpeed/scale", animationScale)
+	set_anim.rpc("parameters/Blend2/blend_amount", animationSpeed)
+	
+@rpc("any_peer", "call_local")
+func set_anim(path, arg):
+	leg_animations.set(path, arg)
+
+@rpc("any_peer", "call_local")
+func set_anim_players(setting: bool):
+	#animation_player.active = setting
+	arm_animations.active = setting
+
+func _on_character_body_3d_grabbed(object: Variant) -> void:
+	if not is_multiplayer_authority(): return
+	grabbed_object = object
